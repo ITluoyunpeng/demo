a3c036bb47a8a44089306cdc60d49271b657c561
luoyunpeng <1825325715@qq.com>
2021-03-04 14:03:17 +0800

    测试mysql连接

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.demo;
 
+import com.example.demo.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @SpringBootTest
 class DemoApplicationTests {
+    @Resource
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     void contextLoads() {
+        String sql="select * from user_info";
+        List<User> query = jdbcTemplate.query(sql, new RowMapper<User>() {
+
+            @Override
+            public User mapRow(ResultSet resultSet, int i) throws SQLException {
+                User user = new User();
+                user.setUser_id(resultSet.getString("user_id"));
+                user.setUser_name(resultSet.getString("user_name"));
+                user.setUser_sex(resultSet.getString("user_sex"));
+                return user;
+            }
+        });
+        for (User user:
+             query) {
+            System.out.println(user.getUser_name());
+        }
     }
 
 }
