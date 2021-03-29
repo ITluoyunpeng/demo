81ad54c319b8a5231b3d53a0664163c4efb15730
luoyunpeng <1825325715@qq.com>
2021-03-29 13:37:02 +0800

    修改

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("ssssss");
+        System.out.println("项目已启动！");
     }
 
 }
